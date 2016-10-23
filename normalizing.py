b3dcff5d0667c74d1c5d793c9935a2f96fe092a8
Vages <eirikvageskar@gmail.com>
2016-10-23 22:18:21 +0200

    Add batch normalization of files

--- normalizing.py
+++ normalizing.py
@@ -8,7 +8,30 @@ def normalize(data_frame):
         data_frame[column] = (data_frame[column] - mean) / standard_deviation
 
 
+def batch_normalize_files(glob_expression, output_folder):
+    import glob
+    import os
+
+    file_paths = glob.glob(glob_expression)
+
+    for i, path in enumerate(file_paths):
+        print "Normalizing file", i + 1, "of", len(file_paths)
+
+        data = pd.read_csv(path)
+        normalize(data)
+
+        file_name = path.split("/")[-1]
+        subject_name = file_name.split("_")[0]
+
+        head, tail = file_name.split(".")
+        subject_output_folder = output_folder + "/" + subject_name
+        output_path = subject_output_folder+ "/" + "".join([head, "_normalized.", tail])
+
+        if not os.path.exists(subject_output_folder):
+            os.makedirs(subject_output_folder)
+
+        data.to_csv(output_path)
+
+
 if __name__ == "__main__":
-    a = pd.read_csv("./DATA/TESTING/004/004_Axivity_BACK_Back.csv", header=None)
-    normalize(a)
-    a.to_csv("./004_Axivity_BACK_Back_normalized.csv", header=False, index=False)
+    batch_normalize_files("./DATA/TRAINING/*/*Axivity*.csv", "./DATA/normalization_output")
